c24a867d1e081f9c9f360222886ea7871435471a
Icass1 <i.cassdolopez@gmail.com>
2023-01-26 12:47:06 +0100

    0007

--- Enigma.py
+++ Enigma.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from time import sleep
 
-print_all_variables = True
+print_all_variables = False
 
 window = Tk()
 
@@ -36,6 +36,7 @@ error_text = StringVar()
 
 out = StringVar() 
 
+
 alphabet_for_position_rotor = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z")
 alphabet =                  ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","Á","É","Í","Ó","Ú","á","é","í","ó","ú","1","2","3","4","5","6","7","8","9",".",","," ","-","+","*","/","¿","?")
 
@@ -49,126 +50,21 @@ _reflector_rotor_external = ("A","B","C","D","E","F","G","H","I","J","K","L","M"
 _reflector_rotor_internal = ("y","+","u","a","P","M","í","*","É","v","1","T","F","Q","q","8","E","N"," ","k","L","3","n","X","W","c","h","D","x","Y","-","Ú",",","m","Z","Í","w","S","4","g","V","é","Ó","7","Ñ","6","t","s","C","J","j","b","A",".","/","I","i","o","e","ó","ñ","G","á","9","K","5","U","l","2","r","p","O","ú","z","f","R","d","B","H","Á","?","¿")
 
 
-class alpha_rotor_external:
-    def __init__(self):
-        self.number = None
-        self.letter = None
 
-    def number_to_letter(self, number):
-        number -=1
-        out = _alpha_rotor_external[number]
-        return out
+def number_to_letter(source, number):
+    number -=1
+    if number < 0: number = 26
+    number_to_letter_out = source[number]
+    return number_to_letter_out
 
-    def letter_to_number(self, letter):
-        counter = 0 
-        for k in _alpha_rotor_external:
-            if k == letter:
-                out = counter 
-                out += 1
-            counter += 1
-        return out
-        
-class alpha_rotor_internal:
-    def __init__(self):
-        self.number = None
-        self.letter = None
-
-    def number_to_letter(self, number):
-        number -=1
-        out = _alpha_rotor_internal[number]
-        return out
-
-    def letter_to_number(self, letter):
-        counter = 0 
-        for k in _alpha_rotor_internal:
-            if k == letter:
-                out = counter 
-                out += 1
-            counter += 1
-        return out
-
-class beta_rotor_external:
-    def __init__(self):
-        self.number = None
-        self.letter = None
-
-    def number_to_letter(self, number):
-        number -=1
-        out = _beta_rotor_external[number]
-        return out
-
-    def letter_to_number(self, letter):
-        counter = 0 
-        for k in _beta_rotor_external:
-            if k == letter:
-                out = counter
-                out += 1
-            counter += 1
-        return out
-
-class beta_rotor_internal:
-    def __init__(self):
-        self.number = None
-        self.letter = None
-
-    def number_to_letter(self, number):
-        number -=1
-        out = _beta_rotor_internal[number]
-        return out
-
-    def letter_to_number(self, letter):
-        counter = 0 
-        for k in _beta_rotor_internal:
-            if k == letter:
-                out = counter
-                out += 1
-            counter += 1
-        return out
-
-class reflector_rotor_external:
-    def __init__(self):
-        self.number = None
-        self.letter = None
-
-    def number_to_letter(self, number):
-        number -=1
-        out = _reflector_rotor_external[number]
-        return out
-
-    def letter_to_number(self, letter):
-        counter = 0 
-        for k in _reflector_rotor_external:
-            if k == letter:
-                out = counter 
-                out += 1
-            counter += 1
-        return out
-
-class reflector_rotor_internal:
-    def __init__(self):
-        self.number = None
-        self.letter = None
-        
-    def number_to_letter(self, number):
-        number -=1
-        out = _reflector_rotor_internal[number]
-        return out
-
-    def letter_to_number(self, letter):
-        counter = 0 
-        for k in _reflector_rotor_internal:
-            if k == letter:
-                out = counter
-                out += 1
-            counter += 1
-        return out
-
-alpha_external = alpha_rotor_external()
-alpha_internal = alpha_rotor_internal()
-beta_external = beta_rotor_external()
-beta_internal = beta_rotor_internal()
-reflector_external = reflector_rotor_external()
-reflector_internal = reflector_rotor_internal()
+def letter_to_number(source, letter):
+    counter = 0 
+    for k in source:
+        if k == letter:
+            letter_to_number_out = counter 
+            letter_to_number_out += 1
+        counter += 1
+    return letter_to_number_out
 
 def letter_up(button):
 
@@ -181,9 +77,9 @@ def letter_up(button):
         
         number_of_letter_1 += 1
 
-        letter_1 = number_to_letter_display(number=number_of_letter_1 + 1)
-        letter_2 = number_to_letter_display(number=number_of_letter_1)
-        letter_3 = number_to_letter_display(number=number_of_letter_1 - 1)
+        letter_1 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_1 + 1)
+        letter_2 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_1)
+        letter_3 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_1 - 1)
 
         value_1=(letter_1)
         value_2=(letter_2)
@@ -198,9 +94,9 @@ def letter_up(button):
         
         number_of_letter_2 += 1
 
-        letter_1 = number_to_letter_display(number=number_of_letter_2 + 1)
-        letter_2 = number_to_letter_display(number=number_of_letter_2)
-        letter_3 = number_to_letter_display(number=number_of_letter_2 - 1)
+        letter_1 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_2 + 1)
+        letter_2 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_2)
+        letter_3 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_2 - 1)
 
         value_1=(letter_1)
         value_2=(letter_2)
@@ -214,9 +110,9 @@ def letter_up(button):
         if number_of_letter_3 > 26: number_of_letter_3 = 0
         
         number_of_letter_3 += 1
-        letter_1 = number_to_letter_display(number=number_of_letter_3 + 1)
-        letter_2 = number_to_letter_display(number=number_of_letter_3)
-        letter_3 = number_to_letter_display(number=number_of_letter_3 - 1)
+        letter_1 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_3 + 1)
+        letter_2 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_3)
+        letter_3 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_3 - 1)
 
         value_1=(letter_1)
         value_2=(letter_2)
@@ -236,9 +132,9 @@ def letter_down(button):
         
         number_of_letter_1 -= 1
 
-        letter_1 = number_to_letter_display(number=number_of_letter_1 + 1)
-        letter_2 = number_to_letter_display(number=number_of_letter_1)
-        letter_3 = number_to_letter_display(number=number_of_letter_1 - 1)
+        letter_1 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_1 + 1)
+        letter_2 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_1)
+        letter_3 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_1 - 1)
 
         value_1 = (letter_1)
         value_2 = (letter_2)
@@ -253,9 +149,9 @@ def letter_down(button):
         
         number_of_letter_2 -= 1
 
-        letter_1 = number_to_letter_display(number = number_of_letter_2+1)
-        letter_2 = number_to_letter_display(number = number_of_letter_2)
-        letter_3 = number_to_letter_display(number = number_of_letter_2-1)
+        letter_1 = number_to_letter(source=alphabet_for_position_rotor, number = number_of_letter_2+1)
+        letter_2 = number_to_letter(source=alphabet_for_position_rotor, number = number_of_letter_2)
+        letter_3 = number_to_letter(source=alphabet_for_position_rotor, number = number_of_letter_2-1)
 
         value_1=(letter_1)
         value_2=(letter_2)
@@ -270,9 +166,9 @@ def letter_down(button):
         
         number_of_letter_3 -= 1
 
-        letter_1 = number_to_letter_display(number=number_of_letter_3+1)
-        letter_2 = number_to_letter_display(number=number_of_letter_3)
-        letter_3 = number_to_letter_display(number=number_of_letter_3-1)
+        letter_1 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_3+1)
+        letter_2 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_3)
+        letter_3 = number_to_letter(source=alphabet_for_position_rotor, number=number_of_letter_3-1)
 
         value_1=(letter_1)
         value_2=(letter_2)
@@ -281,27 +177,6 @@ def letter_down(button):
         input_text_3_1.set(value_1)
         input_text_3_2.set(value_2)
         input_text_3_3.set(value_3)
-
-def number_to_letter(number):
-    number -=1
-    if number < 0: number = 26
-    number_to_letter_out = alphabet[number]
-    return number_to_letter_out
-
-def letter_to_number(letter):
-    counter = 0 
-    for k in alphabet:
-        if k == letter:
-            letter_to_number_out = counter 
-            letter_to_number_out += 1
-        counter += 1
-    return letter_to_number_out
-
-def number_to_letter_display(number):
-    number -=1
-    if number > 26: number = 0
-    out = alphabet_for_position_rotor[number]
-    return out
 
 def reset():
     global number_of_letter_1
@@ -315,9 +190,11 @@ def reset():
     letter_up(button=1)
     letter_up(button=2)
     letter_up(button=3)
-    label.destroy()
+    try: label.destroy()
+    except: pass
 
 def encript():
+    
     global label
     global number_of_letter_1
     global number_of_letter_2
@@ -328,15 +205,65 @@ def encript():
     _position_entry_error = 0
 
     _out = ""
-    _out += number_to_letter(number_of_letter_1) + " " + number_to_letter(number_of_letter_2) + " " + number_to_letter(number_of_letter_3) + " "
+    _out += number_to_letter(source=alphabet, number=number_of_letter_1) + " " + number_to_letter(source=alphabet, number=number_of_letter_2) + " " + number_to_letter(source=alphabet, number=number_of_letter_3) + " "
     for letter in _input:
         if letter in alphabet:
             _position_alpha = number_of_letter_3
             _position_beta = number_of_letter_2
             _position_reflector = number_of_letter_1
 
-            if print_all_variables: 
-                        
+            _letter = int(letter_to_number(source=alphabet, letter=letter))
+            _letter -= _position_alpha
+            _letter += 1
+            if _letter > 82: _letter -= 82
+            elif _letter < 1: _letter += 82
+
+            a = number_to_letter(source=_alpha_rotor_external, number=_letter)
+            
+            b = int(letter_to_number(source=_alpha_rotor_internal, letter=a))
+
+            b += _position_alpha
+            b -= _position_beta
+            if b > 82: b -= 82
+            elif b < 1: b += 82
+
+            c = number_to_letter(source=_beta_rotor_external, number=b)
+
+            d = int(letter_to_number(source=_beta_rotor_internal, letter=c))
+
+            d += _position_beta
+            d -= _position_reflector
+            if d > 82: d -= 82
+            elif d < 1: d += 82
+
+            e = number_to_letter(source=_reflector_rotor_external, number=d)
+
+            f = int(letter_to_number(source=_reflector_rotor_internal, letter=e))
+
+            f-=_position_beta
+            f+=_position_reflector
+            if f > 82: f -= 82
+            elif f < 1: f += 82
+
+            g = number_to_letter(source=_beta_rotor_internal, number=f)
+
+            h = int(letter_to_number(source=_beta_rotor_external, letter=g))
+
+            h -=_position_alpha
+            h +=_position_beta
+            if h > 82: h -= 82
+            elif h < 1: h += 82
+
+            i = number_to_letter(source=_alpha_rotor_internal, number=h)
+
+            j = int(letter_to_number(source=_alpha_rotor_external, letter=i))
+
+            j -= 1
+            j += _position_alpha
+            if j > 82: j -= 82
+            elif j < 1: j += 82
+
+            if print_all_variables:
                 print("\n")
 
                 print("number_of_letter_1: " + str(number_of_letter_1))
@@ -347,68 +274,17 @@ def encript():
                 print("Position beta: " + str(_position_beta)) 
                 print("Position reflector: " + str(_position_reflector))
 
-            _letter = int(letter_to_number(letter=letter))
-            _letter -= _position_alpha
-            _letter += 1
-            if _letter > 82: _letter -= 82
-            elif _letter < 1: _letter += 82
+                print("a: " + str(a))
+                print("b: " + str(b))
+                print("c: " + str(c))
+                print("d: " + str(d))
+                print("e: " + str(e))
+                print("f: " + str(f))
+                print("g: " + str(g))
+                print("h: " + str(h))
+                print("i: " + str(i))
+                print("j: " + str(j))
 
-            a = alpha_external.number_to_letter(number=_letter)
-            if print_all_variables: print("a: " + str(a))
-            
-            b = int(alpha_internal.letter_to_number(letter=a))
-            if print_all_variables: print("b: " + str(b))
-
-            b += _position_alpha
-            b -= _position_beta
-            if b > 82: b -= 82
-            elif b < 1: b += 82
-
-            c = beta_external.number_to_letter(number=b)
-            if print_all_variables: print("c: " + str(c))
-
-            d = int(beta_internal.letter_to_number(letter=c))
-            if print_all_variables: print("d: " + str(d))
-
-            d += _position_beta
-            d -= _position_reflector
-            if d > 82: d -= 82
-            elif d < 1: d += 82
-
-            e = reflector_external.number_to_letter(number=d)
-            if print_all_variables: print("e: " + e)
-
-            f = int(reflector_internal.letter_to_number(letter=e))
-            if print_all_variables: print("f: " + str(f))        
-
-            f-=_position_beta
-            f+=_position_reflector
-            if f > 82: f -= 82
-            elif f < 1: f += 82
-
-            g = beta_internal.number_to_letter(number=f)
-            if print_all_variables: print("g: " + str(g))
-
-            h = int(beta_external.letter_to_number(letter=g))
-            if print_all_variables: print("h: " + str(h))
-
-            h -=_position_alpha
-            h +=_position_beta
-            if h > 82: h -= 82
-            elif h < 1: h += 82
-
-            i = alpha_internal.number_to_letter(number=h)
-            if print_all_variables: print("i: " + str(i))
-
-            j = int(alpha_external.letter_to_number(letter=i))
-            if print_all_variables: print("j: " + str(j))
-
-            j -= 1
-            j += _position_alpha
-            if j > 82: j -= 82
-            elif j < 1: j += 82
-
-            if print_all_variables:
                 print("3: " + str(number_of_letter_3))
                 print("2: " + str(number_of_letter_2))
                 print("1: " + str(number_of_letter_1))
@@ -426,9 +302,8 @@ def encript():
                 number_of_letter_1 = 1
                 number_of_letter_2 = 1
                 number_of_letter_3 = 1
-            
-            
-            _out += number_to_letter(number=j)
+
+            _out += number_to_letter(source=alphabet, number=j)
             out.set(value = (_out))
 
         else: 
@@ -436,8 +311,6 @@ def encript():
             _position_entry_error += 25
             label = Label(window, text=error_text)
             label.place(x=475, y=_position_entry_error)
-
-
 
 letter_up(button=1)
 letter_up(button=2)
@@ -451,7 +324,6 @@ button_down_2 = Button(window, text="DOWN", bg=color_boton, fg=cn, activebackgro
 
 button_up_3 = Button(window, text="UP", bg=color_boton, fg=cn, activebackground=actb, width=ancho_boton, height=alto_boton, command=lambda:letter_up(button=3), cursor="hand2").grid(row=1, column=2, padx=13, pady=10)
 button_down_3 = Button(window, text="DOWN", bg=color_boton, fg=cn, activebackground=actb, width=ancho_boton, height=alto_boton, command=lambda:letter_down(button=3), cursor="hand2").grid(row=5, column=2 , padx=13, pady=10)
-
 
 screen_1_1 = Entry(window, font=("arial",20,"bold"), width=2, textvariable=input_text_1_1, borderwidth=0, background="white", justify="right").grid(row=2, column=0, padx=20, pady=20)
 screen_1_2 = Entry(window, font=("arial",20,"bold"), width=2, textvariable=input_text_1_2, borderwidth=0, background="white", justify="right").grid(row=3, column=0, padx=20, pady=20)
@@ -473,8 +345,5 @@ _output_text = Entry(window, font=("arial",15), width=50, textvariable=out, bord
 button_encript = Button(window, text="Encript", bg=color_boton, fg=cn, activebackground=actb, width=ancho_boton, height=alto_boton, command=lambda:encript(), cursor="hand2").place(x=30, y=435)
 
 button_reset = Button(window, text="Reset", bg=color_boton, fg=cn, activebackground=actb, width=ancho_boton, height=alto_boton, command=lambda:reset(), cursor="hand2").place(x=350, y=160)
-
-# error = Entry(window, font=("arial",10,"bold"), width=30, textvariable=error_text, borderwidth=1, background="white", justify="left").place(x=350, y=250)
-
 
 window.mainloop()
